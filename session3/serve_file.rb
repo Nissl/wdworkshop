cfb86ee081cba4eeb57c77387c73cb431367e16e
Chris Lee <chris@cglee.me>
2014-03-05 19:43:42 -0500

    remove single quote around Content-Type

--- session3/serve_file.rb
+++ session3/serve_file.rb
@@ -7,6 +7,6 @@ html = "<html></html>"
 
 loop do
   client = server.accept
-  client.puts "HTTP/1.1 200 OK\r\nServer: Simple Ruby Server\r\nDate: #{Time.now}\r\nContent-Type: 'text/html'\r\nContent-Length: #{html.length}\r\n\r\n#{html}"
+  client.puts "HTTP/1.1 200 OK\r\nServer: Simple Ruby Server\r\nDate: #{Time.now}\r\nContent-Type: text/html\r\nContent-Length: #{html.length}\r\n\r\n#{html}"
   client.close
 end
